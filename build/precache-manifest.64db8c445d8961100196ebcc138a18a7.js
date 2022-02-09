398b372b3ee3e3611f14065ea69b4a5fa1eb9961
Tushar Dixit <82960618+NitroJett@users.noreply.github.com>
2022-02-09 20:13:47 +0530

    Add files via upload

---
+++ build/precache-manifest.64db8c445d8961100196ebcc138a18a7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4dbefc60fd3eb257b1e35392bbea1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a63883e19cf99ef6056f",
+    "url": "/static/css/2.400796ec.chunk.css"
+  },
+  {
+    "revision": "ab59525661aec0c984b5",
+    "url": "/static/css/main.df7e26cb.chunk.css"
+  },
+  {
+    "revision": "a63883e19cf99ef6056f",
+    "url": "/static/js/2.3c097907.chunk.js"
+  },
+  {
+    "revision": "ab59525661aec0c984b5",
+    "url": "/static/js/main.52cec099.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3c9f706c56ef4576ad1eccfdd0531924",
+    "url": "/static/media/img.3c9f706c.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
